2431f607a15a67919a5415b03d032057d76fc4d9
Donaim <3Donaim@gmail.com>
2021-06-17 18:49:42 +0200

    CreateEventForm: prevent very large inputs

--- signup-form-gui/src/CreateEventForm.tsx
+++ signup-form-gui/src/CreateEventForm.tsx
@@ -20,12 +20,12 @@ function CreateEventForm() {
                    <h2>New event</h2>
                    <div className="form-group">
                        <div className="row">
-                           <div className="col-xs-6"><input type="text" className="form-control" name="first_name" placeholder="First Name" required={true} /></div>
-                           <div className="col-xs-6"><input type="text" className="form-control" name="last_name" placeholder="Last Name" required={true} /></div>
+                           <div className="col-xs-6"><input type="text" maxLength={20} className="form-control" name="first_name" placeholder="First Name" required={true} /></div>
+                           <div className="col-xs-6"><input type="text" maxLength={20} className="form-control" name="last_name" placeholder="Last Name" required={true} /></div>
                        </div>
                    </div>
                    <div className="form-group">
-                       <input type="email" className="form-control" name="email" placeholder="Email" required={true} />
+                       <input type="email" maxLength={100} className="form-control" name="email" placeholder="Email" required={true} />
                    </div>
                    <div className="form-group">
                        <input type="date" className="form-control" name="date" required={true} />
