64aa6d7b6a6b421581f2c4f0306112825ec025ba
Donaim <3Donaim@gmail.com>
2021-06-17 23:14:42 +0200

    Move CreateEventForm reducer to a separate file

--- signup-form-gui/src/CreateEventForm.tsx
+++ signup-form-gui/src/CreateEventForm.tsx
@@ -1,34 +1,9 @@
 
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { sendServerRequest } from './serverConnection';
 import { submitCallback } from './CreateEventFormSubmit';
+import { formStateReducer } from './CreateEventFormReducer';
 import * as T from './CreateEventFormTypes';
-
-const initialState: T.FormState = { type: T.FormStateType.ReceivingInput };
-
-function formStateReducer(state: T.FormState = initialState, action: T.FormAction) {
-    if (typeof(action.type) === 'string') {
-        /* NOTE: redux passes actions that are not of type T.FormAction */
-        return state;
-    }
-
-    switch (action.type) {
-        case T.FormActionType.Submit:
-            sendServerRequest("create-event", action.options)
-                    .then((response) => action.handle(response));
-            return {
-                type: T.FormStateType.Submitting,
-            } as T.FormState;
-        case T.FormActionType.ShowStatus:
-            return {
-                type: T.FormStateType.ReportingStatus,
-                text: action.text,
-            } as T.FormState;
-        default:
-            throw new Error("Impossible");
-    }
-}
 
 const formSelector = (callback: any) => (state: T.FormState) => {
     switch (state.type) {
---
+++ signup-form-gui/src/CreateEventFormReducer.ts
@@ -0,0 +1,30 @@
+
+import { sendServerRequest } from './serverConnection';
+import * as T from './CreateEventFormTypes';
+
+const initialState: T.FormState = { type: T.FormStateType.ReceivingInput };
+
+function formStateReducer(state: T.FormState = initialState, action: T.FormAction) {
+    if (typeof(action.type) === 'string') {
+        /* NOTE: redux passes actions that are not of type T.FormAction */
+        return state;
+    }
+
+    switch (action.type) {
+        case T.FormActionType.Submit:
+            sendServerRequest("create-event", action.options)
+                    .then((response) => action.handle(response));
+            return {
+                type: T.FormStateType.Submitting,
+            } as T.FormState;
+        case T.FormActionType.ShowStatus:
+            return {
+                type: T.FormStateType.ReportingStatus,
+                text: action.text,
+            } as T.FormState;
+        default:
+            throw new Error("Impossible");
+    }
+}
+
+export { formStateReducer };
