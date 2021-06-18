36131b3067eae18a4cf29de6be469628c61da193
Donaim <3Donaim@gmail.com>
2021-06-18 09:34:57 +0200

    CreateEventForm: remove unnecessary divs

--- signup-form-gui/src/CreateEventForm/Form.tsx
+++ signup-form-gui/src/CreateEventForm/Form.tsx
@@ -16,13 +16,11 @@ const formTemplate = (callback: CallbackFn) => (body: JSX.Element) => {
 }
 
 const inputTemplate = (callback: CallbackFn) => (submit: JSX.Element) => {
-    return formTemplate(callback)(<div>
+    return formTemplate(callback)(<React.Fragment>
         <h2>New event</h2>
-        <div className="form-group">
-            <div className="row">
-                <div className="col-xs-6"><input type="text" maxLength={20} className="form-control" name="first_name" placeholder="First Name" required={true} /></div>
-                <div className="col-xs-6"><input type="text" maxLength={20} className="form-control" name="last_name" placeholder="Last Name" required={true} /></div>
-            </div>
+        <div className="form-group row">
+            <div className="col-xs-6"><input type="text" maxLength={20} className="form-control" name="first_name" placeholder="First Name" required={true} /></div>
+            <div className="col-xs-6"><input type="text" maxLength={20} className="form-control" name="last_name" placeholder="Last Name" required={true} /></div>
         </div>
         <div className="form-group">
             <input type="email" maxLength={100} className="form-control" name="email" placeholder="Email" required={true} />
@@ -33,7 +31,7 @@ const inputTemplate = (callback: CallbackFn) => (submit: JSX.Element) => {
         <div className="form-group">
             {submit}
         </div>
-    </div>
+    </React.Fragment>
     );
 }
 
