d2eba27e5fd493d01e33ade056b2dfb498a3179d
Donaim <3Donaim@gmail.com>
2021-06-19 23:23:14 +0200

    Add dynamicState tests

---
+++ signup-form-gui/src/dynamicState.test.js
@@ -0,0 +1,15 @@
+
+import { getDynamic, setDynamic } from './dynamicState';
+
+it('introduces value in given execution contex', (done) => {
+    setDynamic('x', 3, () => { expect(getDynamic('x')).toBe(3); done(); });
+});
+
+it('nests correctly', (done) => {
+    setDynamic('x', 5, () => {
+        expect(getDynamic('x')).toBe(5);
+        setDynamic('x', 3, () => expect(getDynamic('x')).toBe(3));
+        expect(getDynamic('x')).toBe(5);
+        done();
+    });
+});
