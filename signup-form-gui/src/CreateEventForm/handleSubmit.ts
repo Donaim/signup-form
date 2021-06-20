7f6dec7b3a07a838e80a4215bc74e4f026d91cc6
Donaim <3Donaim@gmail.com>
2021-06-20 18:08:10 +0200

    Catch fetch error and report it to the user

--- signup-form-gui/src/CreateEventForm/handleSubmit.ts
+++ signup-form-gui/src/CreateEventForm/handleSubmit.ts
@@ -6,7 +6,9 @@ import { createHandler } from '../store/effect';
 const handleSubmitAction = createHandler(ActionType.CreateEventFormSubmit, (action: Action) => {
     if (action.type !== ActionType.CreateEventFormSubmit) throw new Error("createHandler fault");
 
-    sendServerRequest("create-event", action.options).then(action.handle);
+    sendServerRequest("create-event", action.options)
+        .then(action.handle)
+        .catch(err => action.handle(JSON.stringify({ error: "Server unavailable" })));
 });
 
 export { handleSubmitAction };
